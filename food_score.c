5646fc0df85b6b46e7817be3e25d824df7417f20
Thanhcielblue <ThanhdtCE200284@gmail.com>
2025-07-09 16:41:46 +0700

    Update food_score.c

--- food_score.c
+++ food_score.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <window.h>
-int main()
-{
+#include <windows.h>
     int foodX, foodY;          // Tọa độ thức ăn
     int score = 0;             // Điểm số ban đầu
 
@@ -11,7 +9,7 @@ int main()
     int maxHeight = 25;        // Chiều cao khung chơi
     return 0;
     
-}   void generateFood() {
+    void generateFood() {
     foodX = rand() % 79;
     foodY = rand() % 24;
     gotoxy(foodX, foodY);
@@ -33,7 +31,6 @@ int main()
         gotoxy(0, 0);
         printf("Score: %d", score);
     }
-}
     void food_score(int snakeHeadX, int snakeHeadY) {
     checkFoodCollision(snakeHeadX, snakeHeadY);
     displayScore();
